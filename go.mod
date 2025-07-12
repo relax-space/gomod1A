d6b2332db23937cbff33f2899943c55a5598fd0b
xiaoxinmiao <xiaoxm_168@163.com>
2025-07-12 21:24:09 +0800

    v1

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/relax-space/gomod1A
+
+go 1.24.4
+
+require github.com/relax-space/gomod1B/v3 v3.0.0
+
+require github.com/relax-space/gomod1C/v6 v6.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gomod1B_v3 "github.com/relax-space/gomod1B/v3"
+)
+
+func main() {
+	fmt.Printf("gomod1B_v3.MultiB(2, 3)=%d\n", gomod1B_v3.MultiB(2, 3))
+}
